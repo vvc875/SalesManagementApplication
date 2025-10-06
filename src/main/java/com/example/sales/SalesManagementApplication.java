a9962a25d403893916c70237986f214736f9e365
vvc875 <vanchungvu0807@gmail.com>
2025-10-06 15:37:05 +0700

    Hi

---
+++ src/main/java/com/example/sales/SalesManagementApplication.java
@@ -0,0 +1,15 @@
+package com.example.sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesManagementApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SalesManagementApplication.class, args);
+    }
+
+
+}
